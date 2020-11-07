c558c9b52ec4aa101dbe2d8602f1bbead6e7c118
paveltom <paveltom@post.bgu.ac.il>
2020-11-07 19:44:55 +0200

    MultibitOrGate in progress

--- MultiBitAndGate.cs
+++ MultiBitAndGate.cs
@@ -8,33 +8,51 @@ namespace Components
     //Multibit gates take as input k bits, and compute a function over all bits - z=f(x_0,x_1,...,x_k)
     class MultiBitAndGate : MultiBitGate
     {
-        AndGate[] m_AndGates;
-        protected WireSet m_wsInput;
-        public Wire Output { get; protected set; }
-
+        private AndGate[] m_AndGates;
 
         public MultiBitAndGate(int iInputCount)
             : base(iInputCount)
         {
-
-            m_AndGates = new AndGate[iInputCount];
+            m_AndGates = new AndGate[iInputCount - 1];
+            for (int i = 0; i < m_AndGates.Length; i++)
+                m_AndGates[i] = new AndGate();
             m_AndGates[0].ConnectInput1(m_wsInput[0]);
             m_AndGates[0].ConnectInput2(m_wsInput[1]);
+          
 
-            for (int i = 1; i < iInputCount; i++)
+            for (int i = 1; i < (iInputCount-1); i++)
             {
                 m_AndGates[i].ConnectInput1(m_AndGates[i - 1].Output);
                 m_AndGates[i].ConnectInput2(m_wsInput[i + 1]);
             }
 
-            Output = m_AndGates[iInputCount - 1].Output;
-            
+            Output = m_AndGates[iInputCount - 2].Output;            
         }
 
 
         public override bool TestGate()
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < m_wsInput.Size; i++)
+            {
+                m_wsInput[i].Value = 1;
+            }
+            if (Output.Value != 1) return false;
+
+            for (int i = 0; i < m_wsInput.Size; i++)
+            {
+                m_wsInput[i].Value = 0;
+                if (Output.Value != 0) return false;
+                m_wsInput[i].Value = 1;
+            }
+
+            for (int i = 0; i < m_wsInput.Size; i++)
+            {
+                m_wsInput[i].Value = 0;
+            }
+            if (Output.Value != 0) return false;
+
+
+            return true;
         }
     }
 }
--- MultiBitOrGate.cs
+++ MultiBitOrGate.cs
@@ -9,13 +9,25 @@ namespace Components
 
     class MultiBitOrGate : MultiBitGate
     {
-        //your code here
+        private OrGate[] m_OrGates;
+
 
         public MultiBitOrGate(int iInputCount)
             : base(iInputCount)
         {
-            //your code here
+            m_OrGates = new OrGate[iInputCount - 1];
+            for (int i = 0; i < m_OrGates.Length; i++)
+                m_OrGates[i] = new OrGate();
+            m_OrGates[0].ConnectInput1(m_wsInput[0]);
+            m_OrGates[0].ConnectInput2(m_wsInput[1]);
 
+            for (int i = 1; i < (iInputCount - 1); i++)
+            {
+                m_OrGates[i].ConnectInput1(m_OrGates[i - 1].Output);
+                m_OrGates[i].ConnectInput2(m_wsInput[i + 1]);
+            }
+
+            Output = m_OrGates[iInputCount - 2].Output;
         }
 
         public override bool TestGate()
