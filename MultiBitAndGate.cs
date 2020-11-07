13f3461aad47cc95d25507c5a15c13ab6209f741
paveltom <paveltom@post.bgu.ac.il>
2020-11-07 15:50:51 +0200

    MultiBitAndGate in progress

--- MultiBitAndGate.cs
+++ MultiBitAndGate.cs
@@ -8,13 +8,27 @@ namespace Components
     //Multibit gates take as input k bits, and compute a function over all bits - z=f(x_0,x_1,...,x_k)
     class MultiBitAndGate : MultiBitGate
     {
-        //
+        AndGate[] m_AndGates;
+        protected WireSet m_wsInput;
+        public Wire Output { get; protected set; }
+
 
         public MultiBitAndGate(int iInputCount)
             : base(iInputCount)
         {
-            //your code here
 
+            m_AndGates = new AndGate[iInputCount];
+            m_AndGates[0].ConnectInput1(m_wsInput[0]);
+            m_AndGates[0].ConnectInput2(m_wsInput[1]);
+
+            for (int i = 1; i < iInputCount; i++)
+            {
+                m_AndGates[i].ConnectInput1(m_AndGates[i - 1].Output);
+                m_AndGates[i].ConnectInput2(m_wsInput[i + 1]);
+            }
+
+            Output = m_AndGates[iInputCount - 1].Output;
+            
         }
 
 
