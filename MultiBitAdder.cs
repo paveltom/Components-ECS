070282b1442f0d3fcdaa3be43d2d1df3609a3019
paveltom <paveltom@post.bgu.ac.il>
2020-12-14 00:08:36 +0200

    before sleep...

--- MultiBitAdder.cs
+++ MultiBitAdder.cs
@@ -25,6 +25,27 @@ namespace Components
             Input2 = new WireSet(Size);
             Output = new WireSet(Size);
             //your code here
+            Overflow = new Wire();
+            Wire tempCarry = new Wire();
+
+            FullAdder[] tempFullAdder = new FullAdder[Size];
+            for (int i = 0; i < Size; i++)
+                tempFullAdder[i] = new FullAdder();
+
+            tempCarry.Value = 0;
+            tempFullAdder[0].CarryInput.ConnectInput(tempCarry);
+            tempFullAdder[0].ConnectInput1(Input1[0]);
+            tempFullAdder[0].ConnectInput2(Input2[0]);
+            Output[0].ConnectInput(tempFullAdder[0].Output);
+            for (int i = 1; i < Size; i++)
+            {
+                tempCarry = tempFullAdder[i - 1].CarryOutput;
+                tempFullAdder[i].CarryInput.ConnectInput(tempCarry);
+                tempFullAdder[i].ConnectInput1(Input1[i]);
+                tempFullAdder[i].ConnectInput2(Input2[i]);
+                Output[i].ConnectInput(tempFullAdder[i].Output);
+            }
+            Overflow.ConnectInput(tempFullAdder[Size - 1].CarryOutput);
 
         }
 
