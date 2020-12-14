13f353d84a3ccf7749985a5a4c0754f0fd5a58ba
paveltom <paveltom@post.bgu.ac.il>
2020-12-14 12:01:00 +0200

    MultiBitAdder done

--- MyTests.cs
+++ MyTests.cs
@@ -48,44 +48,73 @@ namespace Components
             //Console.WriteLine("Your number decimal representation (expected value: " + userNum2 + "): " + wire2.Get2sComplement());
             //Console.WriteLine("============================================================================");
             //Console.WriteLine();
+            //Console.WriteLine("============================================================================");
+            //Console.WriteLine();
+            //Console.WriteLine("HalfAdder tests:");
+            //HalfAdder ha = new HalfAdder();
+            //Wire in1 = new Wire();
+            //Wire in2 = new Wire();
+            //in1.Value = 0;
+            //in2.Value = 0;
+            //ha.Input1.ConnectInput(in1);
+            //ha.Input2.ConnectInput(in2);
+            //Console.WriteLine(ha.ToString());
+            //ha = new HalfAdder();
+            //in1.Value = 1;
+            //ha.Input1.ConnectInput(in1);
+            //ha.Input2.ConnectInput(in2);
+            //Console.WriteLine(ha.ToString());
+            //ha = new HalfAdder();
+            //in2.Value = 1;
+            //ha.Input1.ConnectInput(in1);
+            //ha.Input2.ConnectInput(in2);
+            //Console.WriteLine(ha.ToString());
+            //ha = new HalfAdder();
+            //in1.Value = 0;
+            //ha.Input1.ConnectInput(in1);
+            //ha.Input2.ConnectInput(in2);
+            //Console.WriteLine(ha.ToString());
+            //Console.WriteLine();
+            //Console.WriteLine("Inner test result: " + ha.TestGate());
+            //Console.WriteLine("============================================================================");
+            //Console.WriteLine();
+            //Console.WriteLine("============================================================================");
+            //Console.WriteLine();
+            //Console.WriteLine("FullAdder tests:");
+            //FullAdder fa = new FullAdder();
+            //Console.WriteLine("Inner test result: " + fa.TestGate());
             Console.WriteLine("============================================================================");
             Console.WriteLine();
-            Console.WriteLine("HalfAdder tests:");
-            HalfAdder ha = new HalfAdder();
-            Wire in1 = new Wire();
-            Wire in2 = new Wire();
-            in1.Value = 0;
-            in2.Value = 0;
-            ha.Input1.ConnectInput(in1);
-            ha.Input2.ConnectInput(in2);
-            Console.WriteLine(ha.ToString());
-            ha = new HalfAdder();
-            in1.Value = 1;
-            ha.Input1.ConnectInput(in1);
-            ha.Input2.ConnectInput(in2);
-            Console.WriteLine(ha.ToString());
-            ha = new HalfAdder();
-            in2.Value = 1;
-            ha.Input1.ConnectInput(in1);
-            ha.Input2.ConnectInput(in2);
-            Console.WriteLine(ha.ToString());
-            ha = new HalfAdder();
-            in1.Value = 0;
-            ha.Input1.ConnectInput(in1);
-            ha.Input2.ConnectInput(in2);
-            Console.WriteLine(ha.ToString());
-            Console.WriteLine();
-            Console.WriteLine("Inner test result: " + ha.TestGate());
-            Console.WriteLine("============================================================================");
-            Console.WriteLine();
-            Console.WriteLine("============================================================================");
-            Console.WriteLine();
-            Console.WriteLine("FullAdder tests:");
-            FullAdder fa = new FullAdder();
-            Console.WriteLine("Inner test result: " + fa.TestGate());
+            Console.WriteLine("MultiBitAdder tests:");
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("Enter the num of bits (hit 'e' to exit): ");
+                string strIn = Console.ReadLine();
+                if (strIn == "e")
+                    loop = false;
+                else
+                {
+                    int numOfBits4 = Convert.ToInt32(strIn);
+                    Console.Write("Enter first num between -" + (int)Math.Pow(2, numOfBits4 - 1) + " and " + ((int)Math.Pow(2, numOfBits4 - 1) - 1) + " to compute: ");
+                    int userNum41 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second num between -" + (int)Math.Pow(2, numOfBits4 - 1) + " and " + ((int)Math.Pow(2, numOfBits4 - 1) - 1) + " to compute: ");
+                    int userNum42 = Convert.ToInt32(Console.ReadLine());
 
 
+                    MultiBitAdder mba = new MultiBitAdder(numOfBits4);
+                    WireSet in1 = new WireSet(numOfBits4);
+                    WireSet in2 = new WireSet(numOfBits4);
+                    in1.Set2sComplement(userNum41);
+                    in2.Set2sComplement(userNum42);
 
+                    mba.ConnectInput1(in1);
+                    mba.ConnectInput2(in2);
+                    Console.WriteLine("Computation: (" + mba.Input1.Get2sComplement() + ") + (" + mba.Input2.Get2sComplement() + ") = (" + mba.Output.Get2sComplement() + ")");
+
+                    Console.WriteLine(mba);
+                }
+            }
 
 
 
